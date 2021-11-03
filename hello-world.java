cd05c2d3c36aced6277d432eb5b5768e5e3e7220
sailorsister <lisa.lindhe2@gmail.com>
2021-11-03 14:40:30 +0100

    hej

--- hello-world.java
+++ hello-world.java
@@ -1,1 +1,13 @@
-test...
+import static javax.swing.JOptionPane.*;
+public class HP {
+    public static void main (String[] arg){
+        String Högskolepoäng;
+        int år;
+        String textÅr;
+        textÅr = showInputDialog("Vilket år går du?");
+        år=Integer.parseInt(textÅr);
+        int HP = (år-1)*60;
+        Högskolepoäng = "Antal högskolepoäng du läst hittills är " + HP;
+        showMessageDialog (null, Högskolepoäng);
+    }
+}
